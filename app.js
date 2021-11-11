687fcbf27c199db39f1f47e9f05df2ef57e5c24d
michael020399 <michael.galan@uconn.edu>
2021-11-10 20:49:26 -0500

    Now we can use both computers

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 document.getElementById("body").style.backgroundColor = "blue";
 
 document.querySelector("body").style.backgroundColor = "red";
+
+document.querySelector("body").style.backgroundColor = "orange";
