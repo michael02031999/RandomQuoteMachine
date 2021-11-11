ba4e3d735ad5a701460d3fa66d094f2cc0bb9355
michael020399 <michael.galan@uconn.edu>
2021-11-11 10:00:04 -0500

    Color picker

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ document.getElementById("body").style.backgroundColor = "blue";
 document.querySelector("body").style.backgroundColor = "red";
 
 document.querySelector("body").style.backgroundColor = "orange";
+
+function clickingFunction () {
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    console.log(red +"," + green + ","+ blue);
+    document.querySelector("body").style.backgroundColor = "rgb("+  red +","+ green +", "+ blue +")";
+}
+
+/*document.querySelector("div").addEventListener("mouseover", function color (){
+    alert("Hello World!")
+})*/
